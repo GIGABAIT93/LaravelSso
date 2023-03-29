ff21285fb599685096cc56a0502cbab899099b62
GIGABAIT93 <freebitcoin93@gmail.com>
2023-03-29 22:36:18 +0300

    Update SsoController.php

--- src/Http/Controllers/SsoController.php
+++ src/Http/Controllers/SsoController.php
@@ -11,15 +11,17 @@ class SsoController extends Controller
 {
     public function login(Request $request)
     {
-
         if ($request->has('token')) {
             $secretKey = config('sso.secret_key');
+            if (strlen($secretKey) !== 32) {
+                return redirect('/login')->withErrors(['sso_error' => 'Secret key length must be 32 characters.']);
+            }
             $cipher = config('app.cipher');
             $encrypter = new Encrypter($secretKey, $cipher);
             $encryptedToken = $request->input('token');
             try {
                 $decryptedToken = $encrypter->decrypt($encryptedToken);
-            } catch (Illuminate\Contracts\Encryption\DecryptException $e) {
+            } catch (\Illuminate\Contracts\Encryption\DecryptException $e) {
                 return redirect('/login');
             }
             $authMarkerData = json_decode($decryptedToken, true);
